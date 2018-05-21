78a47d68f49a2e805e893f5bf11e1bd572f408bf
Jiyong Kim <rlawldyd577@daum.net>
2018-05-21 21:52:13 +0900

    20180521

--- socket/LocalLookup.java
+++ socket/LocalLookup.java
@@ -3,6 +3,19 @@ import java.net.*;
 public class LocalLookup {
 	public static void main(String[] args){
 		
-		여기에 입력
+		InetAddress inetaddr = null;
+		
+		try {
+			inetaddr = InetAddress.getLocalHost();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Hostname: " + inetaddr.getHostName());
+		System.out.println("Fully qualified domainname: " + inetaddr.getCanonicalHostName());
+		
+		System.out.println("IP address: " + inetaddr.getHostAddress());
+		System.out.println("Hash value of ip: " + inetaddr.hashCode());
+	
 	}
 }
